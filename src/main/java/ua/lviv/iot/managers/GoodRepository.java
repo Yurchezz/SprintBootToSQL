fbeebedbb945f92a8792cfd0983fb66086543ae6
Yura Voyevidka <urik.voevidka@gmail.com>
2019-05-13 02:05:48 +0300

    Reboot of All files

--- src/main/java/ua/lviv/iot/managers/GoodRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.iot.managers;
-
-import org.springframework.data.repository.CrudRepository;
-import ua.lviv.iot.models.Good;
-
-public interface GoodRepository
-        extends CrudRepository<Good, Integer> {
-}
--- src/main/java/ua/lviv/iot/models/HouseWifePreferences.java
+++
@@ -1,7 +0,0 @@
-package ua.lviv.iot.models;
-
-public enum HouseWifePreferences {
-    BOOKS,
-    PANS,
-    COSMETICS
-}
